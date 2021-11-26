7912964096c356c641cace26a961165ce64e4fda
GilbertzRivi <62563806+GilbertzRivi@users.noreply.github.com>
2021-11-26 11:47:57 +0100

    Update vidcomp_rc.py

--- vidcomp_rc.py
+++ vidcomp_rc.py
@@ -1,4 +1,4 @@
-import os
+import os, json, subprocess, shlex
 from time import time
 
 path_to_videos = os.getcwd()
@@ -12,5 +12,7 @@ for path in os.listdir(path_to_videos):
         continue
     
     folder, filename = os.path.split(path)
+    accual_bitrate = int(json.loads(subprocess.run(shlex.split(f'ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -print_format json "{path}"'), stdout=subprocess.PIPE).stdout)['streams'][0]['bit_rate'])
+    target_bitrate = accual_bitrate * ((100 - compression_factor)/100)
     newpath = os.path.join(folder, 'compressed_' + filename)
-    os.system(f'ffmpeg -i "{path}" -vcodec h264 -crf {compression_factor} "{newpath}"')
+    os.system(f'ffmpeg -i "{path}" -vcodec h264 -crf {compression_factor} -maxrate {target_bitrate} -bufsize 128M "{newpath}"')
